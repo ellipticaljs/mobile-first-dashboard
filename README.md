fe4a01bef5b159f21c13e834e1b66b1716c3bb22
Scott <sfrancis@misinteractive.com>
2016-05-18 20:41:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+Mobile First Dashboard
+===========================
+
+Installs a default polymer/ellipticaljs dashboard app with mocked data.
+
+# Installation
+
+
+##prerequisites
+
+``` bash
+
+node
+gulp
+bower
+
+```
+
+
+#clone repo
+
+``` bash
+
+git clone https://github.com/ellipticaljs/mobile-first-dashboard.git
+mv ./mobile-first-dashboard  my-project
+cd my-project
+
+```
+
+
+#npm
+
+``` bash
+
+npm install
+gulp init
+bower install
+
+```
+
+
+#tasks
+
+``` bash
+
+gulp start-live-app
+
+```
+
+# Browser
+
+``` bash
+
+localhost:9040
+
+```
+
+## Additional Tasks
+
+``` bash
+
+gulp sass-compile
+gulp app-build-imports
+
+```
+
+
---
+++ gulpfile.js
@@ -0,0 +1,149 @@
+
+var config=require('./config.json');
+var Tasks=require('elliptical-gulp');
+var gulp=require('gulp');
+
+
+var tasks=new Tasks(config);
+
+gulp.task('init',function(){
+    build();
+});
+
+gulp.task('default',function(){
+    tasks.default();
+});
+
+gulp.task('start-live-server',function(){
+    tasks.startLiveServer();
+});
+
+gulp.task('start-server',function(){
+    tasks.startServer();
+});
+
+gulp.task('start-live',function(){
+    tasks.startLive();
+});
+
+gulp.task('start',function(){
+    tasks.start();
+});
+
+gulp.task('start-live-app',function(){
+    tasks.startLiveApp();
+});
+
+gulp.task('start-app',function(){
+    tasks.startApp();
+});
+
+gulp.task('start-live-sass',function(){
+    tasks.startLiveSass();
+});
+
+gulp.task('start-sass',function(){
+    tasks.startSass();
+});
+
+gulp.task('start-live-app-no-sass',function(){
+    tasks.startLiveAppNoSass();
+});
+
+gulp.task('start-app-no-sass',function(){
+    tasks.startAppNoSass();
+});
+
+gulp.task('start-live-scripts',function(){
+    tasks.startLiveScripts();
+});
+
+gulp.task('start-scripts',function(){
+    tasks.startScripts();
+});
+
+gulp.task('sass-compile', function () {
+    tasks.sassCompile();
+});
+
+gulp.task('sass-watch', function () {
+    tasks.sassWatch();
+});
+
+gulp.task('scripts-watch', function () {
+    tasks.scriptsWatch();
+});
+
+gulp.task('scripts-build', function () {
+    tasks.scriptsBuild();
+});
+
+gulp.task('app-watch', function () {
+    tasks.appWatch();
+});
+
+gulp.task('app-build', function () {
+    tasks.appBuild();
+});
+
+gulp.task('vulcanize', function () {
+    tasks.vulcanize();
+});
+
+gulp.task('app-build-imports', function () {
+    tasks.appBuildImports();
+});
+
+gulp.task('app-watch-imports', function () {
+    tasks.appWatchImports();
+});
+
+gulp.task('watch', function () {
+    tasks.watch();
+});
+
+function build(){
+    copyCommonPublicTask();
+    copySassTask();
+    copyIndexTask();
+    copyBowerTask();
+    copyMainAppFileTask();
+    copyAppImportFileTask();
+    copyVulcanized();
+
+}
+
+function copyCommonPublicTask(){
+    gulp.src('./node_modules/dashboard-common/src/public/**/*.*')
+        .pipe(gulp.dest('./public/'));
+}
+
+function copySassTask(){
+    gulp.src('./node_modules/dashboard-common/src/sass/**/*.*')
+        .pipe(gulp.dest('./sass/'));
+}
+
+function copyIndexTask(){
+    gulp.src('./src/public/index.html')
+        .pipe(gulp.dest('./public/'));
+}
+
+function copyBowerTask(){
+    gulp.src('./node_modules/dashboard-common/src/bower.json')
+        .pipe(gulp.dest('./'));
+}
+
+function copyMainAppFileTask(){
+    gulp.src('./src/public/app/app.js')
+        .pipe(gulp.dest('./public/app/'));
+}
+
+function copyAppImportFileTask(){
+    gulp.src('./src/public/imports/app.html')
+        .pipe(gulp.dest('./public/imports/'));
+}
+
+function copyVulcanized(){
+    gulp.src('./src/public/vulcanized/import.html')
+        .pipe(gulp.dest('./public/vulcanized/'));
+}
